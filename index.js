dcec814b97e8757606f9b9090726ec17453c79e7
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-09 17:28:05 -0400

    added put request

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.post("/api/users", (req, res) => {
     res
       .status(400)
       .json({ errorMessage: "Please provide name and bio for the user." });
-  } else if (userData.name || userData.bio) {
+  } else if (userData.name && userData.bio) {
     userData.id = shortid.generate();
     users.push(userData);
     res.status(201).json(userData);
@@ -44,6 +44,56 @@ server.get("/api/users/:id", (req, res) => {
       .json({ message: "The user with the specified ID does not exist." });
   } else if (user.length > 0) {
     res.status(200).json(user);
+  } else {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved."
+    });
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.filter(user => id === user.id);
+
+  if (user.length <= 0) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (user.length > 0) {
+    res.status(200).json({ message: "User Deleted" });
+    let newUser = users.filter(user => id !== user.id);
+    users = newUser;
+  } else {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved."
+    });
+  }
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.filter(user => id === user.id);
+  const userData = req.body;
+
+  if (user.length <= 0) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (user.length > 0) {
+    if (!userData.name || !userData.bio) {
+      res.status(400).json({
+        errorMessage: "Please provide name and bio for the user."
+      });
+    } else if (userData.name && userData.bio) {
+      users = users.map(user => {
+        if (`${user.id}` === id) {
+          userData.id = id
+          return userData;
+        }
+        return user;
+      });
+      res.status(200).json(userData);
+    }
   } else {
     res.status(500).json({
       errorMessage: "The user information could not be retrieved."
