4dabb63e11f5dc871f95ee0b9b23686d26f141e0
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-09 19:44:08 -0400

    created structure to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+
+import {Button} from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [isAdding, setIsAdding] = useState(false);
+  const [users, setUsers] = useState([])
+
+  const getUsers = () => {
+    axios
+      .get("http://localhost:5000/api/users")
+      .then(res => {
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {isAdding ? (
+          <UserForm setIsAdding={setIsAdding} getUsers={getUsers} />
+        ) : (
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setIsAdding(true)}
+          >
+            Add User
+          </Button>
+        )}
+        <UserList users={users} getUsers={getUsers}/>
+      </div>
   );
 }
 
